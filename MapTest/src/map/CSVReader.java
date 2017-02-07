619f7e1eda49cee17d963ed4b4a198daf1bcb284
ulvestad <simenul@stud.ntnu.no>
2017-02-07 14:05:10 +0100

    X Y kordinater konvertert

--- MapTest/src/map/CSVReader.java
+++ MapTest/src/map/CSVReader.java
@@ -9,10 +9,10 @@ public class CSVReader {
 
 	public static void main(String[] args) {
 
-		String csvFile = "/Users/mathiasjornsen/Desktop/uttak_konvertert.csv"; //Filepath for csv file
+		String csvFile = "C:/NTNU/uttak_konvertert.csv"; //Filepath for csv file
 		String line = ""; //Temporary variable for current line
-		String cvsSplitBy = ";"; 
-		
+		String cvsSplitBy = ";";
+
 		//Array to store all coordinates
 		ArrayList<ArrayList<String>> UTMkordinatListe = new ArrayList<ArrayList<String>>();
 		//Array for each set of coordinates
@@ -21,7 +21,7 @@ public class CSVReader {
 		//Variable to skip first line of input data (headers)
 		Boolean firstLine = true;
 
-		//Loop trough the file, adding coordinates to the arraylists. 
+		//Loop trough the file, adding coordinates to the arraylists.
 		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
 			while ((line = br.readLine()) != null) {
 				if (!firstLine) {
@@ -38,10 +38,10 @@ public class CSVReader {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		for (ArrayList<String> list : UTMkordinatListe) {
-			System.out.println(list.get(0) + " " + list.get(1));
+			System.out.println(list.get(0) + "," + list.get(1));
 		}
-		
+
 	}
 }
