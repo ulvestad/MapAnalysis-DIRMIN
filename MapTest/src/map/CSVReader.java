8a78cb8e48f6b84a806337434684b0363527cd4b
ulvestad <simenul@stud.ntnu.no>
2017-02-07 14:38:56 +0100

    done generating images for cordinates!

--- MapTest/src/map/CSVReader.java
+++ MapTest/src/map/CSVReader.java
@@ -5,13 +5,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+
 public class CSVReader {
 
 	public static void main(String[] args) {
 
-		String csvFile = "C:/NTNU/uttak_konvertert2.txt"; //Filepath for csv file
+		String txtFile = "C:/NTNU/uttak_XY.txt"; //Filepath for txt file
 		String line = ""; //Temporary variable for current line
 		String cvsSplitBy = ",";
+		Map map = new Map();
 
 		//Array to store all coordinates
 		ArrayList<ArrayList<String>> UTMkordinatListe = new ArrayList<ArrayList<String>>();
@@ -21,21 +23,27 @@ public class CSVReader {
 		//Variable to skip first line of input data (headers)
 
 		//Loop trough the file, adding coordinates to the arraylists.
-		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(txtFile))) {
 			while ((line = br.readLine()) != null) {
 				// use , as separator
 				String[] cordinates = line.split(cvsSplitBy);
 				kordinater = new ArrayList<String>();
-				kordinater.add(cordinates[3]);
-				kordinater.add(cordinates[4]);
+				kordinater.add(cordinates[0]);
+				kordinater.add(cordinates[1]);
 				UTMkordinatListe.add(kordinater);
 				}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+		int i = 0; //counter for number of images to make
 		for (ArrayList<String> list : UTMkordinatListe) {
 			System.out.println(list.get(0) + "," + list.get(1));
+			map.aerialPhoto(list.get(1), list.get(0), i);
+			if(i+1>20){
+				break;
+			}
+			i++;
 		}
 
 	}
--- MapTest/src/map/Map.java
+++ MapTest/src/map/Map.java
@@ -12,12 +12,13 @@ import javax.swing.JLabel;
 
 public class Map {
 
-	public static void main(String[] args) {
-		JFrame test = new JFrame("Google Maps");
+
+	public void aerialPhoto(String x, String y, int count){
+
 
 		try {
-			String imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center=66.119075893897,13.584447360715572&zoom=16&size=600x600&maptype=satellite&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyD8rXXlTRsfEiHBUlP6D-uIOjQPgHhBWtY";
-			String destinationFile = "res/map.jpg";
+			String imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center="+x+","+y+"&zoom=16&size=600x600&maptype=satellite&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyD8rXXlTRsfEiHBUlP6D-uIOjQPgHhBWtY";
+			String destinationFile = "res/map"+count+".jpg";
 			URL url = new URL(imageUrl);
 			InputStream is = url.openStream();
 			OutputStream os = new FileOutputStream(destinationFile);
@@ -36,12 +37,9 @@ public class Map {
 			System.exit(1);
 		}
 
-		test.add(new JLabel(new ImageIcon((new ImageIcon("res/map.jpg")).getImage().getScaledInstance(600,
-				600, java.awt.Image.SCALE_SMOOTH))));
 
-		test.setVisible(true);
-		test.pack();
 
 	}
+
 
 }
