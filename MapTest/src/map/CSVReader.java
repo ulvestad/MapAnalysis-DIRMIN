cdefb73b85ee7478158af2b23f90e7edc858408b
Mathias Jørnsen <pajornse@stud.ntnu.no>
2017-02-07 13:46:52 +0100

    La kordinater inn i arrayer

--- MapTest/src/map/CSVReader.java
+++ MapTest/src/map/CSVReader.java
@@ -3,30 +3,45 @@ package map;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CSVReader {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        String csvFile = "C:/NTNU/uttak_konvertert.csv";
-        String line = "";
-        String cvsSplitBy = ";";
+		String csvFile = "/Users/mathiasjornsen/Desktop/uttak_konvertert.csv"; //Filepath for csv file
+		String line = ""; //Temporary variable for current line
+		String cvsSplitBy = ";"; 
+		
+		//Array to store all coordinates
+		ArrayList<ArrayList<String>> UTMkordinatListe = new ArrayList<ArrayList<String>>();
+		//Array for each set of coordinates
+		ArrayList<String> kordinater;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+		//Variable to skip first line of input data (headers)
+		Boolean firstLine = true;
 
-            while ((line = br.readLine()) != null) {
-
-                // use ; as separator
-                String[] cordinates = line.split(cvsSplitBy);
-
-                System.out.println("Sted: "+cordinates[1]+", Kordinater: [UTMEast: " + cordinates[3] + " , UTMNorth:" + cordinates[4] + "]");
-
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
+		//Loop trough the file, adding coordinates to the arraylists. 
+		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+			while ((line = br.readLine()) != null) {
+				if (!firstLine) {
+					// use ; as separator
+					String[] cordinates = line.split(cvsSplitBy);
+					kordinater = new ArrayList<String>();
+					kordinater.add(cordinates[3]);
+					kordinater.add(cordinates[4]);
+					UTMkordinatListe.add(kordinater);
+				} else {
+					firstLine = false;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		for (ArrayList<String> list : UTMkordinatListe) {
+			System.out.println(list.get(0) + " " + list.get(1));
+		}
+		
+	}
 }
