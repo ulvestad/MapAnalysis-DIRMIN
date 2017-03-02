6087f99bff0bf9fe082261a69b9356cb06600b0b
ulvestad <simenul@stud.ntnu.no>
2017-03-02 10:26:12 +0100

    functinal version 0.1 with exe file and electon app

--- UI/js/launchEXE.js
+++ UI/js/launchEXE.js
@@ -1,12 +1,12 @@
 //Vars for launching EXE file
 var child = require('child_process').execFile;
-var executablePath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+var executablePath = "dist/label_image/label_image.exe";
 //Var for list of parameters for EXE file
-var parameters = ["--incognito"];
+var parameters = [""];
 
 //Launches a .EXE file --------------------------------
 function launchProgram(){
-  child(executablePath, parameters, function(err, data) {
+  child(executablePath, function(err, data) {
     if(err){
       console.error(err);
       return;
