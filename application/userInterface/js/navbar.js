9ae11ce86b7dbecc44a06d76601b039aa169b0b2
sander arnesen <sander arnesen>
2017-03-21 14:20:01 +0100

    Created navbar, and started on flexbox grid

---
+++ application/userInterface/js/navbar.js
@@ -0,0 +1,7 @@
+function navbar_open() {
+    document.getElementById("myNavbar").style.display = "block";
+}
+
+function navbar_close() {
+    document.getElementById("myNavbar").style.display = "none";
+}
