66540d3d7e67deb25e169552e104b970c8ad4f3d
ulvestad <simenul@stud.ntnu.no>
2017-03-29 21:28:06 +0200

    added COMPREHENSIVE comments and working UPDATE script

---
+++ application/userInterface/py/updateDB.py
@@ -0,0 +1,9 @@
+#IMPORTS USED BY SCRIPT-------------------------------------------------------------------------------------------------
+import sqlite3
+import sys
+
+#SETS UP CONNECTION AND PERFORMS SQL QUERY TO DATABASE USING SYSTEM_ARGS-------------------------------------------------
+conn = sqlite3.connect('db/QuarryLocations.db');
+conn.execute('UPDATE NewLocations SET Latitude=?, Longitude=? WHERE ID=?',(sys.argv[1],sys.argv[2],sys.argv[3]));
+conn.commit();
+conn.close(); #CLOSE DB CONNETION	
