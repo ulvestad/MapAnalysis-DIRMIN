412a588bab6bd265f2c55588dd2eb10ee2195d7c
Martin Storødegård <Martin Stordegrd>
2017-04-20 21:51:48 +0200

    Added folder select

--- application/userInterface/js/selectFolder.js
+++ application/userInterface/js/selectFolder.js
@@ -1,23 +1,28 @@
-var folderPath = "\\maps";
+//Path to folder, is "none" if it hasn't been set yet
+var folderPath = "none";
 
 function openFolder(){
-	//Let the user chose a folder from the filesystem
+	//Let the user choose a folder from the filesystem
 	function chooseFile(name) {
 		var chooser = document.querySelector(name);
 		chooser.addEventListener("change", function(evt) {
 			folderPath = this.value;
-			getFolderPath();
 		}, false);
 
 		chooser.click();  
 	}
 	chooseFile('#fileDialog');
-	
 }
 
-function getFolderPath (){
-	//Returns the path to the selected folder, but only the correct path if it's located in the 'maps' folder
-	finalFolderpath = "\\maps" + folderPath.toString().split("fakepath")[1];
-	console.log(finalFolderpath);
-	return finalFolderpath;
+function getFolderPath(){
+	if (folderPath === "none"){
+		console.log("no folder selected");
+		return;
+	}else{
+		//Returns the path to the selected folder, but only the correct path if it's located in the 'maps' folder
+		//Splits the "fake" path and saves the important part of it
+		finalFolderpath = "\\maps" + folderPath.toString().split("fakepath")[1];
+		console.log(finalFolderpath);
+		return finalFolderpath;
+	}
 }
