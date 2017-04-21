b8aed2ae7867df1223bc4128d9b7967554f86fe1
Martin Storødegård <Martin Stordegrd>
2017-04-21 11:52:55 +0200

    Select folder feature 3.0

--- application/userInterface/js/selectFolder.js
+++ application/userInterface/js/selectFolder.js
@@ -1,5 +1,5 @@
 //Path to folder, is "none" if it hasn't been set yet
-var folderPath = "none";
+var folderPath = null;
 
 function openFolder(){
 	//Let the user choose a folder from the filesystem
@@ -15,9 +15,8 @@ function openFolder(){
 }
 
 function getFolderPath(){
-	if (folderPath === "none"){
-		console.log("no folder selected");
-		return;
+	if (folderPath == null){
+		return null;
 	}else{
 		//Returns the path to the selected folder, but only the correct path if it's located in the 'maps' folder
 		//Splits the "fake" path and saves the important part of it
