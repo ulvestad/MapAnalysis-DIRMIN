d2331d796da28a6d0f5a02883834a1671673544d
Markus Foss <markusrf@stud.ntnu.no>
2017-04-24 09:26:40 +0200

    Fixed: Now prints meaningful output to textfield.

--- application/userInterface/js/launchEXE.js
+++ application/userInterface/js/launchEXE.js
@@ -8,14 +8,17 @@ function launchProgram(){
 	parameters = [getFolderPath()];
 	if (parameters[0] == null){
 		console.log("Please select folder");
+		document.getElementById("textOutput").value += "Please select a folder to scan first.\n";
 	}else{
+		document.getElementById("textOutput").value += "----------- Process ----------\nStarting quarry recognition, this may take some time.\n";
 		child(executablePath, parameters, function(err, data) {
 			if(err){
 				console.error(err);
+				document.getElementById("textOutput").value += "Some error occured.\n";
 				return;
 			}
 			console.log(data.toString());
-			document.getElementById("textOutput").value += "--------------------- Process --------------------\nStarting quarry recongntion, this may take some time.";
+			document.getElementById("textOutput").value += "Scan completed!\n";
 		});
 	}
 }
