ff0135a2535be9412f369d1b55434937c345a6cb
ulvestad <simenul@stud.ntnu.no>
2017-04-26 15:59:38 +0200

    working export and update

--- application/userInterface/js/exportCSV.js
+++ application/userInterface/js/exportCSV.js
@@ -1,20 +1,7 @@
 
 function exportNewLocaitonsToCSV(){
-		console.log('asd')
-		//var spawn  = require("child_process").spawn; //spawns a childs proc.
-		//var child = spawn('python', ["userInterface/py/exportCSV.py"]); //calls a python script
-		var spawn = require('child_process').spawn,
-		    ls    = spawn('python',['userInterface/py/exportCSV.py']);
 
-		ls.stdout.on('data', function (data) {
-		    console.log('stdout: ' + data);
-		    ls.kill();
-		});
-
-		ls.stderr.on('data', function (data) {
-		    console.log('stderr: ' + data);
-		});
-		console.log('halla')
-
+		var spawn  = require("child_process").spawn; //spawns a childs proc.
+		var child = spawn('python', ["userInterface/py/exportCSV.py"]); //calls a python script
 
 }	
--- application/userInterface/py/exportCSV.py
+++ application/userInterface/py/exportCSV.py
@@ -3,15 +3,13 @@ import sqlite3
 import time
 import os
 
-
-
 conn = sqlite3.connect('db/QuarryLocations.db')
 cursor = conn.execute('SELECT * FROM NewLocations')
 result_set = cursor.fetchall()
 conn.commit()
 conn.close()
 
-filename = "./../../exportedCSV/NewLocations(" +time.strftime("%d.%m.%Y-%H.%M")+ ").csv"
+filename = "exportedCSV/NewLocations(" +time.strftime("%d.%m.%Y-%H.%M")+ ").csv"
 
 dir = os.path.dirname(filename)
 if not os.path.exists(dir):
@@ -24,7 +22,7 @@ with open(filename, "wb") as f:
 		content = [str(row[0]),str(row[1]),str(row[2]),str(row[3]),str(row[4]),str(row[5]),str(row[6]),str(row[7])]
 		writer.writerow(content)
 	#writer.writerows(result_set)
-	print('done')
+
 		
 
 
