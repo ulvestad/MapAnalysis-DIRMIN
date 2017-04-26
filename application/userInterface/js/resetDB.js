e8b7e40bcb3116d7adf5eb738421d3cce24cdc5b
Mathias Jørnsen <pajornse@stud.ntnu.no>
2017-04-26 18:41:10 +0200

    Changed a link to child process

--- application/userInterface/js/resetDB.js
+++ application/userInterface/js/resetDB.js
@@ -2,7 +2,7 @@
 function deleteAllRowsFromNewAndPossbileInDB(){
 	if(confirm('Are you sure you want to reset the database? \nNB: All rows in NewLocations and PossibleLocations will be deleted.')){
 		var spawn  = require("child_process").spawn; //spawns a childs proc.
-		var child = spawn('python',["../py/resetDB.py"]);
+		var child = spawn('python',["userInterface/py/resetDB.py"]);
 	} else{
 		return;
 	}
