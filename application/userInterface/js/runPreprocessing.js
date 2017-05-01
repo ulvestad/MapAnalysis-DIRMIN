e5c4860dc0f0949d1562d008d8e6e55997b462d3
ulvestad <simenul@stud.ntnu.no>
2017-05-01 19:05:15 +0200

    trying to fix child proc

--- application/userInterface/js/runPreprocessing.js
+++ application/userInterface/js/runPreprocessing.js
@@ -36,14 +36,14 @@ function preProcessing() {
 	getAllxml(path);
 	console.log("Starting image slicing on " + path);
 	var child = require('child_process').execFile;
-	var executablePath = "userInterface/py/map_slicer.exe";
+	var executablePath = "userInterface/js/map_slicer.exe";
 	parameters = [path];
 	child(executablePath, parameters, function(err, data) {
-		if(err){
-			console.error(err);
-			return;
-		}
-		console.log(data.toString());
+	//	if(err){
+	//		console.error(err);
+	//		return;
+	//	}
+	//	console.log(data.toString());
 	});
 	//child.stdout.on('data', function(data) {
 	//	if (data.toString().trim() === "slicing done") {
