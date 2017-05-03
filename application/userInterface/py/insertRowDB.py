6c7f676c3bbf0c714e13cca0db60600d8c2ee762
ulvestad <simenul@stud.ntnu.no>
2017-05-03 10:43:08 +0200

    created two scripts for inserting row and deleting row in DB

--- application/userInterface/py/insertRowDB.py
+++ application/userInterface/py/insertRowDB.py
@@ -3,7 +3,7 @@ import sqlite3
 import sys
 #SETS UP CONNECTION AND PERFORMS SQL QUERY TO DATABASE USING SYSTEM_ARGS-------------------------------------------------
 conn = sqlite3.connect('db/QuarryLocations.db');
-conn.execute('DELETE FROM ? WHERE ID = ?',(sys.argv[1]),sys.argv[1]); #deletes a specific row
+conn.execute('INSERT INTO ? (ID,UTMZone,UTMEast,UTMNorth) VALUES (null,?,?,?)',(sys.argv[1], sys.argv[2], sys.argv[3],sys.argv[4],sys.argv[5],,sys.argv[6],,sys.argv[7],,sys.argv[8],sys.argv[9]))
 conn.commit();
 conn.close(); #CLOSE DB CONNETION
 sys.exit(0)
