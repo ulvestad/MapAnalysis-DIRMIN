fd6a3b981181e497c7486d6fd6091bc3b8470548
Martin Storødegård <Martin Stordegrd>
2017-05-03 13:56:03 +0200

    Final ver listView result page

--- application/userInterface/py/insertRowDB.py
+++ application/userInterface/py/insertRowDB.py
@@ -5,6 +5,5 @@ import sys
 conn = sqlite3.connect('db/QuarryLocations.db');
 conn.execute('INSERT INTO NewLocations (ID,FileName,UTMZone,UTMEast,UTMNorth, UTMSouth, UTMWest, Score) VALUES (null,?,?,?,?,?,?,?)',(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7]))
 conn.commit();
-print("insert works")
 conn.close(); #CLOSE DB CONNETION
 sys.exit(0)
