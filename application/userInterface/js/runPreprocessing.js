645a170131a87f175a17b9645c6e844a7fe99676
Martin Storødegård <Martin Stordegrd>
2017-05-06 17:17:57 +0200

    preprocess several files at once and progress bar

--- application/userInterface/js/runPreprocessing.js
+++ application/userInterface/js/runPreprocessing.js
@@ -3,10 +3,12 @@ var folderPath = null;
 //Used to make sure the chooseFile function is only run once, since it makes an event listener every 
 //time the user clicks the "select folder" button
 var selectFolder = true;
+var folderQueue = [];
 
 function openFolder(){
 	//Let the user choose a folder from the filesystem
-	function chooseFile(name) {
+	function chooseFile() {
+		var name = '#fileDialog'
 		selectFolder = true;
 		var chooser = document.querySelector(name);
 		//Creates an event listener when file browsing is opened, is executed when user selects folder
@@ -22,15 +24,22 @@ function openFolder(){
 				}
 				//document.getElementById("textOutput").value += "Directory '" + folderPath + "' selected.\n";
 				split_path = folderPath.split("\\");
-				partial_path = split_path[split_path.length -2 ] +"/" +split_path[split_path.length -1] + "/"
+				partial_path = split_path[split_path.length -2 ] +"/" +split_path[split_path.length -1] + "/";
 				document.getElementById("folderPathSelected").value =  "  ✔ "+partial_path;
-				document.getElementById("textOutput").value += "Starting pre-processing...\n";
-				preProcessing();
+				document.getElementById("textOutput").value += "Folder Selected\n";
+				//List of selected folders
+				folderQueue.push(getFolderPath());
+				//All selected folders shown in textfield
+				document.getElementById("folderPathSelected").value = '';
+				for (x=0; x<folderQueue.length; x++){
+					var pathPieces = folderQueue[x].split("\\");
+					document.getElementById("folderPathSelected").value +="  ✔ " + pathPieces[pathPieces.length-2] + "/" +  pathPieces[pathPieces.length-1] + '\r';
+				}
 			}
 		}, false);
 		chooser.click();
 	}
-	chooseFile('#fileDialog');
+	chooseFile();
 }
 
 // Returns the currently selected folder
@@ -41,37 +50,74 @@ function getFolderPath(){
 		return folderPath;
 	}
 }
+function clearFolderList(){
+	document.getElementById("folderPathSelected").value = '';
+	folderQueue = [];
+}
 
 // This function is run after a folder has been selected.
 // Gets the selected folder path and launches the map_slicer.py python script
 // that slices the images into 12 new ones, as well as reading all metadata and
 // adding the data to the database
+
 function preProcessing() {
-	// Gets the selected folder
-	path = getFolderPath();
-	path = path.split("\\").join("/");
-	console.log(path);
-
-	// Reads metadata and add to database
-	getAllxml(path);
-
-	// Begins the process of slicing images
-	console.log("Starting image slicing on " + path);
-	var child = require('child_process').execFile;
-	var executablePath = "userInterface/py/dist/map_slicer/map_slicer.exe";
-	parameters = [path];
-	child(executablePath, parameters, function(err, data) {
-		if(err){
-			console.error(err);
+	var x= 0;
+	document.getElementById("pFolderProgress").innerHTML  = "Folders scanned: (" + 0 + "/" + folderQueue.length + ")";
+	disableButtons(true);
+	function preProcessingLoop(){
+		if (folderQueue.length <= x){
+			document.getElementById("pFolderProgress").innerHTML  = "Folders scanned: " + "(" + x + "/" + x + ") Done";
+			x = 0;
+			clearFolderList();
+			console.log("Completed!");
+			disableButtons(false);
+			
 			return;
 		}
-		console.log(data.toString());
-		document.getElementById("textOutput").value += "Pre-processing completed!\n";
-	});
-	//child.stdout.on('data', function(data) {
-	//	if (data.toString().trim() === "slicing done") {
-	//		document.getElementById("textOutput").value += "Image slicing complete!\n";
-	//	}
+		// Gets the selected folder
+		path = folderQueue[x];
+		path = path.split("\\").join("/");
 
-	//});
+		// Reads metadata and add to database
+		getAllxml(path);
+
+		// Begins the process of slicing images
+		var child = require('child_process').execFile;
+		var executablePath = "userInterface/py/dist/map_slicer/map_slicer.exe";
+		parameters = [path];
+		child(executablePath, parameters, function(err, data) {
+			if(err){
+				console.error(err);
+				return;
+			}
+			console.log(data.toString());
+			document.getElementById("textOutput").value += "Pre-processing completed!\n";
+			if (x < folderQueue.length){
+				x += 1;
+				document.getElementById("pFolderProgress").innerHTML  = "Folders scanned: (" + x + "/" + folderQueue.length + ")";
+				preProcessingLoop();
+			}
+		});
+	}
+	preProcessingLoop();
+}
+
+function disableButtons(disable){
+	document.getElementById("btnOpenFolder").disabled = disable;
+	document.getElementById("btnClearFolderList").disabled = disable;
+	document.getElementById("btnPreProcessing").disabled = disable;
+	document.getElementById("btnLaunchProgram").disabled = disable;
+	document.getElementById("btnOpenCSVFile").disabled = disable;
+	document.getElementById("btnOpenFolderCSV").disabled = disable;
+	document.getElementById("btnWipeDBAndMaps").disabled = disable;
+	if(disable){
+		btnNavIndex.href = "#";
+		btnNavResults.href = "#";
+		console.log("All buttons disabled");
+	}else{
+		//Note: These paths of the nav-links are static, so may have to change it later
+		btnNavIndex.href = "index.html";
+		btnNavResults.href = "resultPage.html";
+		console.log("All buttons enabled");
+	}
 }
