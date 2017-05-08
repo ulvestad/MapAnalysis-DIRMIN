67bd4f3ef475cbcb5892b64bdfd36a9d499606a1
sander arnesen <sander arnesen>
2017-05-08 19:32:08 +0200

    Design on pages

--- application/userInterface/js/slider.js
+++ application/userInterface/js/slider.js
@@ -4,7 +4,7 @@ var lowGlobalThreshold = 0.9;
 var highGlobalThreshold = 1;
 //Runs once to check amount of quarries within threshold and display on button
 var locationsInThreshold = checkQuarryLength(lowGlobalThreshold, highGlobalThreshold);
-document.getElementById("showQuarries").innerHTML = ("Show quarries (" + locationsInThreshold + ")");
+document.getElementById("showQuarries").innerHTML = ("Show Quarries (" + locationsInThreshold + ")");
 //Creates a new slider with some parameters
 noUiSlider.create(slider, {
 	start: [0.9, 1],
@@ -26,9 +26,9 @@ slider.noUiSlider.on('update', function(){
 	//Looks through the PossibleLocations-table continuously while the slider is dragged and updates the list and number
 	//If slow, put these lines into the "set" function below instead
 	locationsInThreshold = checkQuarryLength(lowGlobalThreshold, highGlobalThreshold);
-	document.getElementById("showQuarries").innerHTML = ("Show quarries (" + locationsInThreshold + ")");
+	document.getElementById("showQuarries").innerHTML = ("Show Quarries (" + locationsInThreshold + ")");
 
-	
+
 })
 
 //Changes the label on the show quarries button ever time the slider value has changed
