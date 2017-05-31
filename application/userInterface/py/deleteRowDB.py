99523037392d5deca837df68dbbb4773530a6f47
Mathias Jørnsen <pajornse@stud.ntnu.no>
2017-05-31 12:23:42 +0200

    Comments for deleteRowDB.py

--- application/userInterface/py/deleteRowDB.py
+++ application/userInterface/py/deleteRowDB.py
@@ -1,9 +1,30 @@
-#IMPORTS USED BY SCRIPT-------------------------------------------------------------------------------------------------
+"""
+
+    Filename: userInterface/py/deleteRowDB.py
+    @Author: Group 13
+
+    Description: Deletes a specific row from PossibleLocations table in the database.
+
+    Globals:
+        conn - Database connection
+
+    Input:
+        ID for the row to be deleted.
+
+    Output:
+        Updated database
+
+"""
+
+# Imports required
 import sqlite3
 import sys
-#SETS UP CONNECTION AND PERFORMS SQL QUERY TO DATABASE USING SYSTEM_ARGS-------------------------------------------------
+
+# Makes connection to database
 conn = sqlite3.connect('db/QuarryLocations.db');
 conn.execute('DELETE FROM PossibleLocations WHERE ID = ?',[sys.argv[2]]); #deletes a specific row
 conn.commit();
-conn.close(); #CLOSE DB CONNETION
+
+# Close db connection
+conn.close();
 sys.exit(0)
